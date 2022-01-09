b49df3f98f226ca9d5c1fc14b3c864031161c2de
mumitrols <mumitrols@bk.ru>
2022-01-09 14:50:40 +0300

    добавлен код решения

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        //входные данные
+        float priceTicket = 300.75F;
+        int priceMile = 20;
+
+        int totalMiles = (int) priceTicket / priceMile;
+        System.out.println(totalMiles);
+
+    }
+}
